a7353e4dcb4f5d6e3030fc9e23cfe0bc6bfdd408
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2021-01-15 10:06:10 +0100

    First commit

---
+++ src/vae.py
@@ -0,0 +1,161 @@
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+from tensorflow.python.keras import Input, Model
+from tensorflow.python.keras.callbacks import CSVLogger
+from tensorflow.python.keras.layers import Conv2D, Dense, Flatten, \
+    Conv2DTranspose, Reshape, BatchNormalization, Dropout
+from tensorflow.python.keras.optimizers import Adam
+from tensorflow.python.ops.losses.losses_impl import absolute_difference, Reduction
+
+import numpy as np
+
+class Sampling(tf.keras.layers.Layer):
+    def call(self, inputs, **kwargs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+
+def encoder():
+    latent_dim = 512
+    encoder_inputs = Input(shape=(32, 32, 90))
+
+    x = Conv2D(128, 5, activation='relu', strides=2, padding="same")(encoder_inputs)
+    x = BatchNormalization()(x)
+
+    x = Conv2D(256, 5, activation="relu", strides=2, padding="same")(x)
+    x = BatchNormalization()(x)
+
+    x = Conv2D(512, 5, activation="relu", strides=2, padding="same")(x)
+    x = BatchNormalization()(x)
+
+    x = Flatten()(x)
+
+    z_mean = Dropout(0.25)(Dense(latent_dim, activation='relu', name="z_mean")(x))
+    z_log_var = Dropout(0.25)(Dense(latent_dim, activation='relu', name="z_log_var")(x))
+
+    z = Sampling()([z_mean, z_log_var])
+
+    encoder = Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+    return encoder
+
+
+def decoder():
+    latent_inputs = keras.Input(shape=(512,))
+    x = Dropout(0.25)(Dense(8 * 8 * 512, activation='relu')(latent_inputs))
+    x = Reshape((8, 8, 512))(x)
+
+    x = Conv2DTranspose(512, 1, activation='relu', padding='same')(x)
+    x = BatchNormalization()(x)
+
+    x = Conv2DTranspose(256, 3, strides=2, activation='relu', padding="same")(x)
+    x = BatchNormalization()(x)
+
+    x = Conv2DTranspose(128, 3, strides=2, activation='relu', padding="same")(x)
+    x = BatchNormalization()(x)
+
+    decoder_outputs = Conv2DTranspose(90, 3, activation='sigmoid', padding="same")(x)
+
+    decoder = Model(latent_inputs, decoder_outputs, name="decoder")
+    return decoder
+
+
+def dicriminative_error(error, mask):
+    mask1 = 1 - mask
+    error1 = tf.math.multiply(error, mask1)
+    N1 = tf.reduce_sum(mask1, axis=[1, 2])
+    mean = tf.math.divide(tf.reduce_sum(error1, axis=[1, 2]), N1)
+    return mean
+
+
+class vae(keras.Model):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(vae, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def call(self, inputs, **kwargs):
+        _, _, z = self.encoder(inputs)
+        reconstruction = self.decoder(z)
+        return inputs, reconstruction
+
+    def train_step(self, data):
+        if isinstance(data, tuple):
+            data = data[0]
+            mask = data[1]
+
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
+
+            L1 = absolute_difference(data, reconstruction, reduction=Reduction.NONE)
+            error = tf.reduce_mean(L1, axis=3)
+
+            reconstruction_loss = dicriminative_error(error, mask)
+
+            reconstruction_loss = tf.reduce_mean(reconstruction_loss)
+
+            kl_loss = -0.5*tf.reduce_mean(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+
+            total_loss = reconstruction_loss + kl_loss
+
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+        return {
+            "loss": total_loss,
+            "reconstruction_loss": reconstruction_loss,
+            "kl_loss": kl_loss,
+        }
+
+    def test_step(self, data):
+        if isinstance(data, tuple):
+            data = data[0]
+            mask = data[1]
+
+        z_mean, z_log_var, z = self.encoder(data)
+        reconstruction = self.decoder(z)
+
+        L1 = absolute_difference(data, reconstruction, reduction=Reduction.NONE)
+        error = tf.reduce_mean(L1, axis=3)
+
+        reconstruction_loss = dicriminative_error(error, mask)
+
+        reconstruction_loss = tf.reduce_mean(reconstruction_loss)
+        kl_loss = -0.5 * tf.reduce_mean(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+
+        total_loss = reconstruction_loss + kl_loss
+
+        return {
+            "loss": total_loss,
+            "reconstruction_loss": reconstruction_loss,
+            "kl_loss": kl_loss,
+        }
+
+
+def train(name_model, dataPath, maskPath):
+    data = np.load(dataPath)
+    mask = np.load(maskPath)
+
+    train_data, test_data, train_mask, test_mask = train_test_split(data, mask, random_state=42)
+
+    model = vae(encoder(), decoder())
+    model.compile(optimizer=Adam(lr=1e-6))
+
+    checkpoint = tf.keras.callbacks.ModelCheckpoint("../models/{}".format(name_model),
+                                                    monitor='val_loss', verbose=1,
+                                                    save_best_only=True, mode='min')
+    csv_logger = CSVLogger("{}.csv".format(name_model), append=True)
+
+    callbacks_list = [checkpoint, csv_logger]
+
+    model.fit(train_data, train_mask, epochs=250, batch_size=128,
+              validation_data=(test_data, test_mask),
+              callbacks=callbacks_list)
+
+
+if __name__ == '__main__':
+    print('yo')
---
+++ srm/prepData.py
@@ -0,0 +1,121 @@
+import cv2
+import os
+import skimage
+import numpy as np
+from tqdm import tqdm
+
+
+def load_images(path_img, path_msk):
+    spliced, copy_moved = [], []
+    names = os.listdir(path_img)
+    names.sort()
+    spliced_msk, copy_moved_msk = [], []
+    for name in tqdm(names):
+        mask_name = name[:-4] + "_gt.png"
+        msk = cv2.imread(path_msk + mask_name, 0)
+        img = cv2.imread(path_img + name, 1)
+        if msk is not None:
+            msk = cv2.flip(msk, 1)
+            Nx_msk, Ny_msk = msk.shape
+            Nx_img, Ny_img, _ = img.shape
+            if Nx_msk != Nx_img:
+                img = cv2.transpose(img)
+            Nx_msk, Ny_msk = msk.shape
+            Nx_img, Ny_img, _ = img.shape
+            if Nx_msk == Nx_img and Ny_msk == Ny_img:
+                if name.split(".")[-1].lower() in {"jpeg", "jpg", "png", 'tif'}:
+                    if name[3] == 'D':
+                        spliced.append(img[..., ::-1])
+                        spliced_msk.append(msk[..., ::-1])
+                    else:
+                        copy_moved.append(img[..., ::-1])
+                        copy_moved_msk.append(msk[..., ::-1])
+    return spliced, copy_moved, spliced_msk, copy_moved_msk
+
+
+def patch_images(images, masks):
+    data, labels = [], []
+    for n, image in enumerate(tqdm(images)):
+        patchs_img = extractPatches(image, (32, 32, 3), 8)
+        patchs_msk = extractPatchesMask(masks[n], (32, 32), 8)
+        for k, patch_img in enumerate(patchs_img):
+            patch_msk = patchs_msk[k]
+            data.append(patch_img)
+            labels.append(patch_msk)
+    return data, labels
+
+
+def extractPatches(im, window_shape, stride):
+    patches = skimage.util.view_as_windows(im, window_shape, stride)
+    nR, nC, t, H, W, C = patches.shape
+    nWindow = nR * nC
+    patches = np.reshape(patches, (nWindow, H, W, C))
+
+    return patches
+
+
+def extractPatchesMask(msk, window_shape, stride):
+    patches = skimage.util.view_as_windows(msk, window_shape, stride)
+    nR, nC, H, W = patches.shape
+    nWindow = nR * nC
+    patches = np.reshape(patches, (nWindow, H, W))
+
+    return patches
+
+
+def applySRM(img, srmkernel):
+    lst_srm = []
+    for k in range(30):
+        filter_srm = srmkernel[:,:,0,k]
+        filtered = cv2.filter2D(img,-1,filter_srm)
+        lst_srm.append(filtered)
+    return np.concatenate(lst_srm, axis=-1)
+
+
+def filter_data():
+    srm = np.loadtxt('rich_model.txt')
+    srmkernel = np.float32(srm)
+    srmkernel = np.reshape(srmkernel, [30, 1, 5, 5])
+    srmkernel = np.transpose(srmkernel, (2, 3, 1, 0))
+
+    img = np.load('PATH')
+    n, _, _, _ = img.shape
+    img_srm = []
+    for k in range(n):
+        img_srm.append(applySRM(img[k], srmkernel))
+    np.save('PATH', img_srm)
+
+
+def save_data():
+    path_img, path_msk = "../data/CASIA2/Tp/", "../data/CASIA2/gt/"
+
+    spliced, copy_moved, spliced_msk, copy_moved_msk = load_images(path_img, path_msk)
+    data, labels = patch_images(spliced, spliced_msk)
+
+    np.save("../data/CASIA.numpy/spliced.npy", data)
+    np.save("../data/CASIA.numpy/spliced_msk.npy", labels)
+
+    np.save("../data/CASIA.numpy/copymoved.npy", copy_moved)
+    np.save("../data/CASIA.numpy/copymoved_msk.npy", copy_moved_msk)
+
+    np.save("../data/CASIA.numpy/all.npy", np.concatenate((spliced, copy_moved)))
+    np.save("../data/CASIA.numpy/all_msk.npy", np.concatenate((spliced_msk, copy_moved_msk)))
+
+
+def extract_no_border():
+    data = np.load("../data/CASIA.numpy/all.npy")
+    labels = np.load("../data/CASIA.numpy/all_msk.npy")
+
+    n, _, _ = labels.shape
+    inside = []
+
+    for k in range(n):
+        s = np.sum(labels[k])
+        if s == 0 or s == 32*32:
+            inside.append(data[k])
+
+    np.svae("../data/CASIA.numpy/inside.npy", inside)
+
+
+if __name__ == '__main__':
+    print("yo")
