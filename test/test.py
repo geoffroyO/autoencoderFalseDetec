5b32f25cec7bd7ddeb99f8f93332bfe938f5edd9
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2021-01-18 18:39:16 +0100

    ok

--- test/test.py
+++ test/test.py
@@ -2,8 +2,9 @@ import numpy as np
 import cv2
 from tqdm import tqdm
 import sys
-sys.path.append('../src/vae.py')
-import vae as vae
+
+sys.path.append('../src/')
+import src.vae as vae
 
 
 def predendVae4K(model, img, block_size):
@@ -12,18 +13,18 @@ def predendVae4K(model, img, block_size):
 
     blocks = []
     print("... Creating blocks")
-    for i in tqdm(range(0, N-block_size+1, block_size)):
-        for j in range(0, M-block_size+1, block_size):
-            blocks.append(img[i:(i+block_size), j:(j+block_size)])
+    for i in tqdm(range(0, N - block_size + 1, block_size)):
+        for j in range(0, M - block_size + 1, block_size):
+            blocks.append(img[i:(i + block_size), j:(j + block_size)])
 
     error = model.predict(np.array(blocks))
 
     count = 0
     print("... Prediction for each blocks")
-    for i in tqdm(range(0, N-block_size+1, block_size)):
-        for j in range(0, M-block_size+1, block_size):
+    for i in tqdm(range(0, N - block_size + 1, block_size)):
+        for j in range(0, M - block_size + 1, block_size):
             mask_error_pred = error[count]
-            mask_error[i:(i+block_size), j:(j+block_size)] += mask_error_pred
+            mask_error[i:(i + block_size), j:(j + block_size)] += mask_error_pred
 
             count += 1
 
