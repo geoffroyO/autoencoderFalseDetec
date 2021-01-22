b2756f50017076eb9b5eff1735f29dba86b85658
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2021-01-22 14:19:26 +0100

    ok

--- test/curve.py
+++ test/curve.py
@@ -13,32 +13,31 @@ from scipy.ndimage.measurements import label
 from tqdm import tqdm
 from skimage import io
 
-precision, recall = [0 for _ in range(5)], [0 for _ in range(5)]
 
-for count, noise in tqdm(enumerate([20, 40, 60, 80, 100])):
-    for k in tqdm(range(1, 49)):
-        pred_msk, msk = io.imread("./lnoise/{}/".format(k) + "{}_gt.png".format(noise), as_gray=True), io.imread("./lnoise/{}/gt.png".format(k), as_gray=True)
-        n, m = np.shape(pred_msk)
-        fp, tp, fn = 0, 0, 0
-        for i in range(n):
-            for j in range(m):
-                pred, true = pred_msk[i, j], msk[i, j]/255
-                if pred and not true:
-                    fp += 1
-                elif pred and true:
-                    tp += 1
-                elif not pred and true:
-                    fn += 1
-        if tp+fp != 0:
-            precision[count] += tp/(tp+fp) 
-        else:
-            precision[count] += 0
-        if tp+fn != 0:
-                recall[count] += tp/(tp+fn)
-        recall[count] += 0
-        
-    precision[count] /= 49
-    recall[count] /= 49   
+def dice(pred_mask, mask):
+    n, m = mask.shape
+    count_pred, count_ori = 0, 0
+    inter = 0
+    for i in range(n):
+        for j in range(m):
+            pred = pred_mask[i, j]
+            ori = mask[i, j]
+            
+            count_pred += pred
+            count_ori += ori
+            
+            if pred and ori:
+                inter += 1
+    return 2*inter/(count_pred+count_ori)
 
-np.save("./precision.npy", np.array(precision))
-np.save("./recall.npy", np.array(recall))
+if __name__ == '__main__:
+    d = [0 for _ in range(5)]
+    for count, noise in tqdm(enumerate([20, 40, 60, 80, 100])):
+        for k in tqdm(range(1, 49)):
+            pred_msk, msk = io.imread("./lnoise/{}/".format(k) + "{}_gt.png".format(noise), as_gray=True), io.imread("./lnoise/{}/gt.png".format(k), as_gray=True)
+            
+            dcount[count] += dice(pred_msk, msk)
+        dcount[count] /= 49
+
+
+    np.save("./dice.npy", np.array(d))
